f66785d973ffa41af9a0ef61403ca025cb6ae749
iLightThings <gitgub@ggray.info>
2023-03-20 08:11:15 -0400

    some updates

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import ntpath
 import socket
 
 from impacket import smb, smb3, nmb, nt_errors, LOG
-from impacket.ntlm import compute_lmhash, compute_nthash
+from impacket.ntlm import compute_lmhash, compute_nthash, NTLMSSP_NEGOTIATE_NTLM, getNTLMSSPType1
 from impacket.smb3structs import SMB2Packet, SMB2_DIALECT_002, SMB2_DIALECT_21, SMB2_DIALECT_30, GENERIC_ALL, FILE_SHARE_READ, \
     FILE_SHARE_WRITE, FILE_SHARE_DELETE, FILE_NON_DIRECTORY_FILE, FILE_OVERWRITE_IF, FILE_ATTRIBUTE_NORMAL, \
     SMB2_IL_IMPERSONATION, SMB2_OPLOCK_LEVEL_NONE, FILE_READ_DATA , FILE_WRITE_DATA, FILE_OPEN, GENERIC_READ, GENERIC_WRITE, \
     FILE_OPEN_REPARSE_POINT, MOUNT_POINT_REPARSE_DATA_STRUCTURE, FSCTL_SET_REPARSE_POINT, SMB2_0_IOCTL_IS_FSCTL, \
     MOUNT_POINT_REPARSE_GUID_DATA_STRUCTURE, FSCTL_DELETE_REPARSE_POINT, FSCTL_SRV_ENUMERATE_SNAPSHOTS, SRV_SNAPSHOT_ARRAY, \
     FILE_SYNCHRONOUS_IO_NONALERT, FILE_READ_EA, FILE_READ_ATTRIBUTES, READ_CONTROL, SYNCHRONIZE, SMB2_DIALECT_311, SMB2Negotiate_Response, \
-    SMB2_NEGOTIATE_SIGNING_ENABLED, SMB2_NEGOTIATE_SIGNING_REQUIRED
+    SMB2_NEGOTIATE_SIGNING_ENABLED, SMB2_NEGOTIATE_SIGNING_REQUIRED, SMB2SessionSetup, SMB2_SESSION_SETUP
+from impacket.spnego import SPNEGO_NegTokenInit, TypesMech
+
 
 
 from impacket.smbconnection import SMBConnection
@@ -21,6 +23,8 @@ class AuditTarget:
         self.TargetIP = TargetIP
         self.canConnect = False
         self.SupportedSMBDilects = []
+        self.SupportsNTLMv1 = False
+        self.SupportsLanMan = False
         self.SigningEnabled = False
         self.SigningRequired = False
         self.NBName = ""
@@ -28,6 +32,7 @@ class AuditTarget:
         self.DNSName = ""
         self.DNSDomain = ""
         self.NTLMSupport = True
+        self._Connection = ""
 
 
     def print(self):
@@ -40,6 +45,7 @@ class AuditTarget:
     def negotiatie(self):
         try:
             smbconn = SMBConnection(remoteHost=self.TargetIP,remoteName=self.TargetIP)
+            self._Connection = smbconn
             self.canConnect = True
             if type(smbconn._SMBConnection ) == smb3.SMB3:
                 #smbconn.negotiateSession()
@@ -64,7 +70,10 @@ class AuditTarget:
 
             #Neat Trick, It is okay if the login fails, as we only need to get the NTLMSSP AV data from the server NTLMSSP Challenge
             try:
-                smbconn.login(user="",password="")
+                smbconn.login(user="administrator",password="admin123+")
+                #smbconn._SMBConnection.login(user="",password="")
+           
+                print("a")
             except Exception as e:
                 if "STATUS_NOT_SUPPORTED" in str(e):
                     self.NTLMSupport = False
@@ -73,6 +82,12 @@ class AuditTarget:
             self.DNSDomain = smbconn.getServerDNSDomainName()
             self.NBDomain = smbconn.getServerDomain()
             self.NBName = smbconn.getServerName()
+            self.ServerOS = smbconn._SMBConnection._Session['ServerOS']
+            if smbconn._SMBConnection._Connection['ServerCapabilities']& NTLMSSP_NEGOTIATE_NTLM == NTLMSSP_NEGOTIATE_NTLM: # Not Correct Flags
+                self.SupportsNTLMv1 = True
+
+            
+
 
         except Exception as e:
             
@@ -89,8 +104,43 @@ class AuditTarget:
             try:
                 smbconn = SMBConnection(remoteHost=self.TargetIP,remoteName=self.TargetIP,preferredDialect=i)
                 self.SupportedSMBDilects.append(i)
+
+                if smb.SMB_DIALECT in self.SupportedSMBDilects: # SMBv1 will support NTLMv1
+                    self.SupportsNTLMv1 = True
             except:
                 continue
+
+    def test_NTLMv1(self):
+        smbconn = SMBConnection(remoteHost=self.TargetIP,remoteName=self.TargetIP)
+        try:
+            smbconn.login(user="administrator",password="admin123+")
+        except:
+            pass
+
+        ntlmssp_session = SMB2SessionSetup()
+        if self.SigningRequired == True:
+            ntlmssp_session["SecurityMode"] = SMB2_NEGOTIATE_SIGNING_REQUIRED
+        else:
+            ntlmssp_session["SecurityMode"] = SMB2_NEGOTIATE_SIGNING_ENABLED
+
+        ntlmssp_session['Flags'] = 0
+        blob = SPNEGO_NegTokenInit()
+        blob['MechTypes'] = [TypesMech['NTLMSSP - Microsoft NTLM Security Support Provider']]
+        auth = getNTLMSSPType1(smbconn['ClientName'],"", smbconn['RequireSigning'])
+        blob['MechToken'] = auth.getData()
+
+        ntlmssp_session['SecurityBufferLength'] = len(blob)
+        ntlmssp_session['Buffer']               = blob.getData()
+
+        packet = self._Connection.SMB_PACKET()
+        packet['Command'] = SMB2_SESSION_SETUP
+        packet['Data']    = ntlmssp_session
+
+        smbconn._Session['PreauthIntegrityHashValue'] = smbconn['PreauthIntegrityHashValue']
+        packetID = smbconn.sendSMB(packet)
+        ans = smbconn.closerecvSMB(packetID)        
+
+
 
 
 
@@ -112,6 +162,7 @@ def main():
     t = AuditTarget(smbtarget)
     t.negotiatie()
     t.test_dialects()
+    t.test_NTLMv1()
     t.print()
     print(t)
 
--- readme.md
+++ readme.md
@@ -37,3 +37,51 @@ NetBios Domain: DOMAIN
 
 [-] NetNTLMv1:              True
 ```
+
+
+## Sylabus
+|Wireshark|MS-DOC|
+-----
+|Negotiate NTLM Key| if set, requests usage of the NTLM v1 session security protocol.
+|Negotiate NT only|
+|
+
+
+NTLMSSP_NEGOTIATE_NTLM - WS (ntlmssp.negotiatentlm)
+H (1 bit): If set, requests usage of the NTLM v1 session security protocol. NTLMSSP_NEGOTIATE_NTLM MUST be set in the NEGOTIATE_MESSAGE to the server and the CHALLENGE_MESSAGE to the client. An alternate name for this field is NTLMSSP_NEGOTIATE_NTLM.
+
+
+
+NTLMSSP_REQUEST_NON_NT_SESSION_KEY - WS (ntlmssp.requestnonntsession)
+If set, requests the usage of the LMOWF. An alternate name for this field is NTLMSSP_REQUEST_NON_NT_SESSION_KEY.
+
+
+ NTLMSSP_NEGOTIATE_LM_KEY - (ntlmssp.negotiatelmkey)
+ if set, requests LAN Manager (LM) session key computation. NTLMSSP_NEGOTIATE_LM_KEY and NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY are mutually exclusive. If both NTLMSSP_NEGOTIATE_LM_KEY and NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY are requested, NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY alone MUST be returned to the client. NTLM v2 authentication session key generation MUST be supported by both the client and the DC in order to be used, and extended session security signing and sealing requires support from the client and the server to be used. An alternate name for this field is NTLMSSP_NEGOTIATE_LM_KEY.
+
+
+### SMB Client NTLMv1 with SMB2
+/etc/samba/smb.conf
+```
+[global]
+   client min protocol = SMB2
+   client max protocol = SMB2
+   server min protocol = SMB2
+   server max protocol = SMB2
+   ntlm auth = ntlmv1-permitted
+#   client lanman auth = yes
+   client NTLMv2 auth = no
+## Browsing/Identification ###
+```
+
+```
+sudo smbclient \\\\192.168.1.204\\C$ -U light.local/administrator --password "admin123+" 
+```
+
+if NTLMv1 is enabled, you will be able to connect.
+
+If NTLMv1 is disabled, you will get this error.
+```
+session setup failed: NT_STATUS_LOGON_FAILURE
+
+
